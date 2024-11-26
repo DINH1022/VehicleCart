a1caf395f13b874d55b4e0a0e241fb486c61df9a
22120433 <lequangvinh.127.2022@gmail.com>
2024-11-26 16:04:49 +0700

    create user

---
+++ backend/controllers/userController.js
@@ -0,0 +1,29 @@
+import User from '../models/userModel.js';
+import asyncHandler from '../middlewares/asyncHandler.js';
+
+const createUser = asyncHandler(async(req,res)=>{
+    const {username, email, password} = req.body;
+    if (!username || !email || !password){
+        throw new Error('Please fill all fields');
+    }
+    const userExists = await User.findOne({email});
+    if (userExists){
+        res.status(400).send('User already exists');
+    }
+
+    const newUser = new User({
+        username,
+        email,
+        password
+    });
+
+    try {
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(400)
+        throw new Error('Invalid user data');
+    }
+})
+
+export {createUser};

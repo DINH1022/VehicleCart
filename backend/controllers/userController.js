5fb20a34a184e391fb412610961454afc39a60ba
22120433 <lequangvinh.127.2022@gmail.com>
2024-11-26 21:02:25 +0700

    login user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,7 @@
 import User from '../models/userModel.js';
 import asyncHandler from '../middlewares/asyncHandler.js';
+import bcrypt from 'bcryptjs';
+import createToken from '../utils/createToken.js'
 
 const createUser = asyncHandler(async(req,res)=>{
     const {username, email, password} = req.body;
@@ -11,19 +13,48 @@ const createUser = asyncHandler(async(req,res)=>{
         res.status(400).send('User already exists');
     }
 
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password,salt);
+
     const newUser = new User({
         username,
         email,
-        password
+        password:hashedPassword
     });
 
     try {
         await newUser.save();
-        res.status(201).json(newUser);
+        createToken(res,newUser._id)
+        res.status(201).json({
+            _id: newUser._id,
+            username: newUser.username,
+            email: newUser.email,
+            isAdmin : newUser.isAdmin,
+        });
     } catch (error) {
         res.status(400)
         throw new Error('Invalid user data');
     }
 })
 
-export {createUser};
+const loginUser = asyncHandler(async(req,res)=>{
+    const {email,password} = req.body;
+
+    const existingUser = await User.findOne({email});
+
+    if(existingUser) {
+        const isPasswordCorrect = await bcrypt.compare(password,existingUser.password);
+        if(isPasswordCorrect){
+            createToken(res,existingUser._id);
+            res.status(201).json({
+            _id: existingUser._id,
+            username: existingUser.username,
+            email: existingUser.email,
+            isAdmin : existingUser.isAdmin,
+        });
+        return;
+        }
+    }
+})
+
+export {createUser, loginUser};

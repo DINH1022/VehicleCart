6d70779fcd8a4c81de79e37eea6fe9c5d98468be
22120433 <lequangvinh.127.2022@gmail.com>
2024-11-26 21:13:08 +0700

    logout user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,33 +3,33 @@ import asyncHandler from '../middlewares/asyncHandler.js';
 import bcrypt from 'bcryptjs';
 import createToken from '../utils/createToken.js'
 
-const createUser = asyncHandler(async(req,res)=>{
-    const {username, email, password} = req.body;
-    if (!username || !email || !password){
+const createUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
         throw new Error('Please fill all fields');
     }
-    const userExists = await User.findOne({email});
-    if (userExists){
+    const userExists = await User.findOne({ email });
+    if (userExists) {
         res.status(400).send('User already exists');
     }
 
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password,salt);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = new User({
         username,
         email,
-        password:hashedPassword
+        password: hashedPassword
     });
 
     try {
         await newUser.save();
-        createToken(res,newUser._id)
+        createToken(res, newUser._id)
         res.status(201).json({
             _id: newUser._id,
             username: newUser.username,
             email: newUser.email,
-            isAdmin : newUser.isAdmin,
+            isAdmin: newUser.isAdmin,
         });
     } catch (error) {
         res.status(400)
@@ -37,24 +37,29 @@ const createUser = asyncHandler(async(req,res)=>{
     }
 })
 
-const loginUser = asyncHandler(async(req,res)=>{
-    const {email,password} = req.body;
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
 
-    const existingUser = await User.findOne({email});
+    const existingUser = await User.findOne({ email });
 
-    if(existingUser) {
-        const isPasswordCorrect = await bcrypt.compare(password,existingUser.password);
-        if(isPasswordCorrect){
-            createToken(res,existingUser._id);
+    if (existingUser) {
+        const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+        if (isPasswordCorrect) {
+            createToken(res, existingUser._id);
             res.status(201).json({
-            _id: existingUser._id,
-            username: existingUser.username,
-            email: existingUser.email,
-            isAdmin : existingUser.isAdmin,
-        });
-        return;
+                _id: existingUser._id,
+                username: existingUser.username,
+                email: existingUser.email,
+                isAdmin: existingUser.isAdmin,
+            });
+            return;
         }
     }
 })
 
-export {createUser, loginUser};
+const logoutUser = asyncHandler(async (req, res) => {
+    res.cookie('jwt', '', { httpOnly: true, expires: new Date(0) });
+    res.status(200).json({ message: 'Logged out successfully' });
+})
+
+export { createUser, loginUser, logoutUser };

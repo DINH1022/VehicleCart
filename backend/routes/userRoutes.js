920d1a8cd70cdbbe2237207330d69489b097fb6d
22120433 <lequangvinh.127.2022@gmail.com>
2024-11-28 16:37:31 +0700

    delete user by id

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createUser, loginUser, logoutUser, getAllUsers, getUserProfile, updateUserProfile } from '../controllers/userController.js'
+import { createUser, loginUser, logoutUser, getAllUsers, getUserProfile, updateUserProfile, deleteUserById } from '../controllers/userController.js'
 import {authenticate, authorizeAdmin} from '../middlewares/authMiddleware.js'
 
 const router = express.Router();
@@ -8,5 +8,8 @@ router.route('/').post(createUser).get(authenticate, authorizeAdmin, getAllUsers
 router.post('/auth', loginUser);
 router.post('/logout', logoutUser);
 router.route('/profile').get(authenticate, getUserProfile).put(authenticate, updateUserProfile);
+
+//admin routes
+router.route('/:id').delete(authenticate, authorizeAdmin,deleteUserById);
 
 export default router;

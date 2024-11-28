0f0ce42ce27f82f1ae87b5f0e2b29284a559fea2
22120433 <lequangvinh.127.2022@gmail.com>
2024-11-28 22:45:42 +0700

    get & update user by id

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,35 @@
-import express from 'express'
-import { createUser, loginUser, logoutUser, getAllUsers, getUserProfile, updateUserProfile, deleteUserById } from '../controllers/userController.js'
-import {authenticate, authorizeAdmin} from '../middlewares/authMiddleware.js'
+import express from "express";
+import {
+  createUser,
+  loginUser,
+  logoutUser,
+  getAllUsers,
+  getUserProfile,
+  updateUserProfile,
+  deleteUserById,
+  getUserById,
+  updateUserById,
+} from "../controllers/userController.js";
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.route('/').post(createUser).get(authenticate, authorizeAdmin, getAllUsers);
-router.post('/auth', loginUser);
-router.post('/logout', logoutUser);
-router.route('/profile').get(authenticate, getUserProfile).put(authenticate, updateUserProfile);
+router
+  .route("/")
+  .post(createUser)
+  .get(authenticate, authorizeAdmin, getAllUsers);
+router.post("/auth", loginUser);
+router.post("/logout", logoutUser);
+router
+  .route("/profile")
+  .get(authenticate, getUserProfile)
+  .put(authenticate, updateUserProfile);
 
 //admin routes
-router.route('/:id').delete(authenticate, authorizeAdmin,deleteUserById);
+router
+  .route("/:id")
+  .delete(authenticate, authorizeAdmin, deleteUserById)
+  .get(authenticate, authorizeAdmin, getUserById)
+  .put(authenticate, authorizeAdmin, updateUserById);
 
 export default router;

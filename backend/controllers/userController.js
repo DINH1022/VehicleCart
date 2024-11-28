f83e1416fc3ed5d8ef60f4ff48376ae85ab318b6
22120433 <lequangvinh.127.2022@gmail.com>
2024-11-28 16:17:22 +0700

    get & put user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -66,4 +66,42 @@ const getAllUsers = asyncHandler(async (req, res) => {
     const users = await User.find({});
     res.json(users);
 });
-export { createUser, loginUser, logoutUser, getAllUsers };
+
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+        res.json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+        })
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
+});
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+    if (user) {
+        user.username = req.body.username || user.username;
+        user.email = req.body.email || user.email;
+        if (req.body.password) {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(req.body.password, salt);
+            user.password = hashedPassword;
+        }
+
+        const updateUser = await user.save();
+
+        res.json({
+            _id: updateUser._id,
+            username: updateUser.username,
+            email: updateUser.email,
+        })
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
+});
+export { createUser, loginUser, logoutUser, getAllUsers, getUserProfile, updateUserProfile };

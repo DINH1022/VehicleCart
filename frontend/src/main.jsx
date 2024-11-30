95e32d41d3a18e8fe7feae64bd3fdb287b688859
22120433 <lequangvinh.127.2022@gmail.com>
2024-11-30 20:30:51 +0700

    navbar UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
+import ReactDOM from "react-dom/client";
 import { createRoot } from 'react-dom/client'
+import {Route, RouterProvider, createRoutesFromElements} from 'react-router'
+import { createBrowserRouter } from 'react-router'
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path = "/" element = {<App />} />
+  )
 )
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router}/>
+);
